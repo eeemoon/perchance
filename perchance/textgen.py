b89a652d801b89e1a83728e770c60c96d4d82cc9
EeeMoonJR <eeemoonart@gmail.com>
2024-06-22 18:48:40 +0300

    add docs

--- perchance/textgen.py
+++ perchance/textgen.py
@@ -6,11 +6,25 @@ from typing import AsyncGenerator
 from playwright.async_api import async_playwright, Request
 
 from . import errors
-from .generator import Generator
+from .aigen import AIGenerator
 from .utils import timeout
 
 
-class TextGenerator(Generator):
+class TextGenerator(AIGenerator):
+    """
+    AI text generator.
+
+    Example usage
+    -------------
+    ```python
+    gen = TextGenerator()
+    prompt = "Write a story about a cat"
+
+    async for chunk in gen.text(prompt):
+        print(chunk)
+    ```
+    """
+
     BASE_URL = "https://text-generation.perchance.org/api"
 
     @classmethod
@@ -42,7 +56,7 @@ class TextGenerator(Generator):
 
                 await frame.click('xpath=//button[@id="generateBtn"]')
 
-                async with timeout(8.0, errors.ConnectionError) as t:
+                async with timeout(20.0, errors.ConnectionError) as t:
                     while not key:
                         await t.tick()
                         await asyncio.sleep(0.1)
@@ -69,46 +83,50 @@ class TextGenerator(Generator):
         prompt: `str`
             Text instruction.
         start_with: `str` | `None`
-            Text to start with.
+            Text to start generation with.
         """
         await self.refresh()
 
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(
-                    TextGenerator.BASE_URL + '/generate',
-                    params={
-                        'userKey': self._key,
-                        '__cacheBust': random.random(),
-                        'requestId': f"aiTextCompletion{random.randint(0, 2**30)}"
-                    },
-                    json={
-                        'generatorName': 'ai-text-generator',
-                        'instruction': prompt,
-                        'instructionTokenCount': 1,
-                        'startWith': start_with or '',
-                        'startWithTokenCount': 1,
-                        'stopSequences': []
-                    }
-                ) as response:
-                    
-                    if not response.ok:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                TextGenerator.BASE_URL + '/generate',
+                params={
+                    'userKey': self._key,
+                    '__cacheBust': random.random(),
+                    'requestId': f"aiTextCompletion{random.randint(0, 2**30)}"
+                },
+                json={
+                    'generatorName': 'ai-text-generator',
+                    'instruction': prompt,
+                    'instructionTokenCount': 1,
+                    'startWith': start_with or '',
+                    'startWithTokenCount': 1,
+                    'stopSequences': []
+                }
+            ) as response:
+                if not response.ok:
+                    try:
                         body = await response.json(content_type=None)
                         status = body['status']
-                        print(body, response.status)
+                    except Exception:
+                        raise errors.ConnectionError()
 
-                        if status == 'invalid_key':
-                            raise errors.AuthError()
-                        elif status == 'invalid_data':
-                            raise errors.BadRequestError()
-                    
+                    if status == 'invalid_key':
+                        raise errors.AuthError()
+                    elif status == 'invalid_data':
+                        raise errors.BadRequestError()
+                    else:
+                        raise errors.ConnectionError()
+                
+                try:
                     async for data_chunk in response.content.iter_any():
                         for line in data_chunk.decode().split('\n\n'):
                             if len(line) == 0:
                                 continue
 
-                            data = json.loads(line[5:])
+                            data: dict = json.loads(line[5:])
                             yield data['text']
-        except Exception:
-            raise errors.ConnectionError()
+                except Exception:
+                    raise errors.ConnectionError()
+            
                             
