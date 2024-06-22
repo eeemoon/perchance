e5d68d47b8d8642b898482385b2f2647456484b4
EeeMoonJR <eeemoonart@gmail.com>
2024-06-22 18:48:28 +0300

    small changes

--- perchance/aigen.py
+++ perchance/aigen.py
@@ -1,36 +1,36 @@
 import aiohttp
 import random
 
-from . import errors
 
-
-class Generator:
+class AIGenerator:
     BASE_URL: str
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._key: str | None = None
 
     @classmethod
     async def _fetch_key(cls) -> str:
+        """Fetch an user key from the website."""
         raise NotImplementedError()
     
     @classmethod
     async def _verify_key(cls, key: str) -> bool:
+        """Verify an user key."""
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(
                     cls.BASE_URL + '/checkVerificationStatus',
                     params={
                         'userKey': key,
-                        '__cacheBust': str(random.random())
+                        '__cacheBust': random.random()
                     }
                 ) as response:
                     return 'not_verified' not in await response.text()
         except Exception:
-            raise errors.ConnectionError()
+            return False
             
     async def refresh(self) -> None:
-        """Refresh auth key from the website."""
+        """Verify and refresh the user key if needed."""
         cls = type(self)
         if not self._key or not await cls._verify_key(self._key):
             self._key = await cls._fetch_key()
