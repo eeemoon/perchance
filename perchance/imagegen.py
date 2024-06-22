c0e3bd81b1463a3e7c8bfa67b42eda5247c2d0cb
EeeMoonJR <eeemoonart@gmail.com>
2024-06-22 18:48:14 +0300

    add ImageResponse

--- perchance/imagegen.py
+++ perchance/imagegen.py
@@ -1,3 +1,4 @@
+import aiofiles
 import aiohttp
 import asyncio
 import io
@@ -6,11 +7,106 @@ from playwright.async_api import async_playwright, Request
 from typing import Literal
 
 from . import errors
-from .generator import Generator
+from .aigen import AIGenerator
 from .utils import timeout
 
 
-class ImageGenerator(Generator):
+class ImageResponse:
+    def __init__(
+        self, 
+        *, 
+        generator: "ImageGenerator",
+        image_id: str,
+        file_ext: str,
+        seed: int,
+        prompt: str,
+        width: int,
+        height: int,
+        guidance_scale: float,
+        negative_prompt: str | None,
+        maybe_nsfw: bool
+    ):
+        self._generator: ImageGenerator = generator
+        self._raw_image: io.BytesIO | None = None
+
+        self.image_id: str = image_id
+        self.file_ext: str = file_ext
+        self.seed: int = seed
+        self.prompt: str = prompt
+        self.width: int = width
+        self.height: int = height
+        self.guidance_scale: float = guidance_scale
+        self.negative_prompt: str | None = negative_prompt
+        self.maybe_nsfw: bool = maybe_nsfw
+    
+    def __str__(self) -> str:
+        return f"{self.image_id}.{self.file_ext}"
+    
+    async def __aenter__(self) -> "ImageResponse":
+        return self
+    
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if self._raw_image:
+            self._raw_image.close()
+
+    @property
+    def size(self) -> tuple[int, int]:
+        """Size of the image."""
+        return self.width, self.height
+
+    async def download(self) -> io.BytesIO:
+        """Download the image."""
+        if self._raw_image is None:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                    ImageGenerator.BASE_URL + '/downloadTemporaryImage',
+                    params={
+                        'imageId': self.image_id
+                    }
+                ) as response:
+                    try:
+                        raw = await response.content.read()
+                        image = io.BytesIO(raw)
+                        image.seek(0)
+                        self._raw_image = image
+                    except Exception:
+                        raise errors.ConnectionError()
+                    
+        return self._raw_image
+                
+    async def save(self, filename: str | None = None) -> None:
+        """
+        Download and save the image.
+
+        Parameters
+        ----------
+        filename: `str` | `None`
+            Name of the output file.
+        """
+        file = filename or f"{self.image_id}.{self.file_ext}" 
+
+        async with aiofiles.open(file, 'wb') as f:
+            img = await self.download()
+            await f.write(img.read())
+            
+
+class ImageGenerator(AIGenerator):
+    """
+    AI image generator.
+
+    Example usage
+    -------------
+    ```python
+    gen = ImageGenerator()
+    prompt = "A cat sitting on stairs"
+
+    async with await gen.image(prompt) as result:
+        raw = await result.download()
+        image = Image.open(raw)
+        image.show()
+    ```
+    """
+
     BASE_URL = "https://image-generation.perchance.org/api"
 
     @classmethod
@@ -43,7 +139,7 @@ class ImageGenerator(Generator):
 
                 await frame.click('xpath=//button[@id="generateButtonEl"]')
 
-                async with timeout(8.0, errors.ConnectionError) as t:
+                async with timeout(20.0, errors.ConnectionError) as t:
                     while not key:
                         await t.tick()
                         await asyncio.sleep(0.1)
@@ -62,7 +158,7 @@ class ImageGenerator(Generator):
         seed: int = -1,
         shape: Literal['portrait', 'square', 'landscape'] = 'square',
         guidance_scale: float = 7.0
-    ) -> io.BytesIO:
+    ) -> ImageResponse:
         """
         Generate image.
 
@@ -81,55 +177,61 @@ class ImageGenerator(Generator):
         """
         await self.refresh()
 
-        try:
-            image_id: str | None = None
+        image_id: str | None = None
 
-            if shape == 'portrait':
-                resolution = '512x768'
-            elif shape == 'square':
-                resolution = '512x512'
-            elif shape == 'landscape':
-                resolution = '768x512'
-            else:
-                raise ValueError(f"Invalid shape: {shape}")
+        if shape == 'portrait':
+            resolution = '512x768'
+        elif shape == 'square':
+            resolution = '512x512'
+        elif shape == 'landscape':
+            resolution = '768x512'
+        else:
+            raise ValueError(f"Invalid shape: {shape}")
 
-            async with aiohttp.ClientSession() as session:
-                async with timeout(12.0, errors.ConnectionError) as t:
-                    while image_id is None:
-                        await t.tick()
+        async with aiohttp.ClientSession() as session:
+            async with timeout(20.0, errors.ConnectionError) as t:
+                while image_id is None:
+                    await t.tick()
+                    
+                    async with session.post(
+                        ImageGenerator.BASE_URL + '/generate',
+                        params={
+                            'prompt': prompt,
+                            'negativePrompt': negative_prompt or '',
+                            'userKey': self._key,
+                            '__cache_bust': random.random(),
+                            'seed': seed,
+                            'resolution': resolution,
+                            'guidanceScale': guidance_scale,
+                            'channel': 'ai-text-to-image-generator',
+                            'subChannel': 'public',
+                            'requestId': random.random()
+                        }
+                    ) as response:
+                        try:
+                            body = await response.json(content_type=None)
+                            status = body['status']
+                        except Exception:
+                            raise errors.ConnectionError()
+
+                        if status == 'invalid_key':
+                            raise errors.AuthError()
+                        elif status == 'invalid_data':
+                            raise errors.BadRequestError()
+                        elif status != 'success':
+                            await asyncio.sleep(4.0)
+                            continue
+                
+                        return ImageResponse(
+                            generator=self,
+                            image_id=body['imageId'],
+                            file_ext=body['fileExtension'],
+                            seed=seed,
+                            prompt=prompt,
+                            width=body['width'],
+                            height=body['height'],
+                            guidance_scale=guidance_scale,
+                            negative_prompt=negative_prompt,
+                            maybe_nsfw=body['maybeNsfw']
+                        )
                         
-                        async with session.post(
-                            ImageGenerator.BASE_URL + '/generate',
-                            params={
-                                'prompt': prompt,
-                                'negativePrompt': negative_prompt or '',
-                                'userKey': self._key,
-                                '__cache_bust': random.random(),
-                                'seed': seed,
-                                'resolution': resolution,
-                                'guidanceScale': guidance_scale,
-                                'channel': 'ai-text-to-image-generator',
-                                'subChannel': 'public',
-                                'requestId': random.random()
-                            }
-                        ) as response:
-                            if 'invalid_key' in await response.text():
-                                raise ValueError("Invalid key")
-
-                            data = await response.json(content_type=None)
-                            try:
-                                image_id = data['imageId']
-                            except KeyError:
-                                await asyncio.sleep(4.0)
-
-                async with session.get(
-                    ImageGenerator.BASE_URL + '/downloadTemporaryImage',
-                    params={
-                        'imageId': image_id
-                    }
-                ) as response:
-                    image = io.BytesIO(await response.content.read())
-                    image.seek(0)
-                    return image    
-        except Exception:
-            raise errors.ConnectionError()
